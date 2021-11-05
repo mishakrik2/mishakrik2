1c20d8529690b3799d0c5d9fb2883cf0d2879ac7
mishakrik2 <93202474+mishakrik2@users.noreply.github.com>
2021-11-05 16:23:24 +0200

    Update front-bootstrap.sh

--- front-bootstrap.sh
+++ front-bootstrap.sh
@@ -5,12 +5,12 @@
 
 # Install s3fs dependencies.
 
-amazon-linux-extras install -y epel nginx1;
-yum install -y s3fs-fuse;
+amazon-linux-extras install -y epel nginx1
+yum install -y s3fs-fuse
 
 # Create directory for a mount point.
 
-mkdir /mnt/s3fs;
+mkdir /mnt/s3fs
 
 # Mount s3 bucket to the point.
 
@@ -33,10 +33,26 @@ cp /mnt/s3fs/default.conf /etc/nginx/conf.d/default.conf
 cp /mnt/s3fs/nginx.conf /etc/nginx/nginx.conf
 cp /mnt/s3fs/www.conf /etc/php-fpm.d/www.conf
 
+chown -R nginx:nginx /var/lib/php/session
+
 systemctl start nginx
 systemctl enable nginx
 systemctl start php-fpm
 systemctl enable php-fpm
+
+# Install MariaDB
+
+amazon-linux-extras install -y mariadb10.5
+
+systemctl mariadb start
+systemctl mariadb enable
+
+# Install PMA
+
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz -P /usr/share/nginx/html/
+tar xvzf /usr/share/nginx/html/phpMyAdmin-latest-all-languages.tar.gz -C /usr/share/nginx/html/
+mv '/usr/share/nginx/html/phpMyAdmin-5.1.1-all-languages' '/usr/share/nginx/html/phpmyadmin'
+rm -rf /usr/share/nginx/html/phpMyAdmin-latest-all-languages.tar.gz
 
 # Retrieve static file.
 
